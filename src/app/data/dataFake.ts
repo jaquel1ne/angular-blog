0fde6f5e9aed44165e1988f1ebc36ffaec0ba66d
jaquel1ne <jaquilinas@gmail.com>
2023-05-20 01:05:10 -0300

    personal configs

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id": "1",
-    "title": "Novo Homem de Ferro em 3D",
-    "description": "Marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "May 2023 Streaming Guide",
+    "description": "Check in our list",
+    "photoCover": "https://m.media-amazon.com/images/M/MV5BYjVkNTFkYWItN2FkYS00NjZiLTg0ODYtNDZmMjc0ZDgyOGRkXkEyXkFqcGdeQXVyMTE0MzQwMjgz._V1_QL75_UY281_CR84,0,500,281_.jpg"
   },
   {
     "id": "2",
-    "title": "Novas séries no Disney+",
-    "description": "bla bla bla",
-    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Here are the best 10 best shows of the year so far",
+    "description": "Succession, Poker Face, Yellowjackets and more...",
+    "photoCover": "https://catracalivre.com.br/wp-content/uploads/2023/04/fotos-catraca-2023-04-10t180645498.png"
+  },
+  {
+    "id": "3",
+    "title": "Great albums for your list",
+    "description": "2023 is already music blessed",
+    "photoCover": "https://pyxis.nymag.com/v1/imgs/672/163/cbc460abd4bc131c29c95d29a56519774f-best-of-the-year-so-far-2023-albums.rhorizontal.w700.jpg"
+  },
+  {
+    "id": "4",
+    "title": "We’ve rounded up our favorite books of the year thus far",
+    "description": "Whether you’re into novels, short stories, memoirs, or nonfiction, there’s something here for every type of reader",
+    "photoCover": "https://i0.wp.com/www.horrorhomeroom.com/wp-content/uploads/2023/03/header1.png?fit=1000%2C668&ssl=1"
   }
 ]
